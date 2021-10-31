2333ea2ebd3179eddcb6b20cb5ddf1590e1d9c20
Florencia Quiñonez <fquinonez@uade.edu.ar>
2021-10-31 20:50:18 -0300

    Agrego restricciones.

--- logica_del_juego.py
+++ logica_del_juego.py
@@ -38,57 +38,102 @@ def interpretar_coordenadas(coord):
     '''Recibe una cadena de coordendas alfanumerica,
     las interpreta en numeros y las devuelve en forma de tupla'''
     # Separar la coordenada en letra a y numero b
-    a = coord[0].lower()
-    b = int(coord[1])
+    if coord[0].isalpha():
+        a = coord[0].lower()
+        b = int(coord[1])
+    else:
+        a = coord[1].lower()
+        b = int(coord[0])
     # Creo un diccionario para interpretar a
     letras = ["a", "b", "c", "d", "e", "f", "g", "h"]
     dic_letras = {letras[num]: num for num in range(8)}
     return (dic_letras[a], b - 1)
 
 
-def ingresar_coordenadas():
+def limpiar_coordenadas(coord):
+    '''Elimina espacios y caracteres no alfanumericos
+    del sting que se pasa como parametro.
+    Devuelve un string con los caracteres restantes.'''
+    coord = coord.lower()
+    coord_l = "".join([caracter for caracter in coord if caracter.isalnum()])
+    return coord_l
+
+
+def ingresar_coordenadas(valor_maximo):
     '''El usuario ingresa coordenadas por teclado en
     formato string'''
-    coordenadas = input("Ingrese las coordenadas")
-    # TODO Restricciones y excepciones
-    # TODO No permitir ingresar valores repetidos
-    return coordenadas
+    msj_error = "Las coordenadas ingresadas son incorrectas.\nEl formato es, por ejemplo, 'A1'."
+    coord = input("Ingrese las coordenadas: ")
+    # Paso los caracteres a minúsculas, elimino valores no alfanumericos
+    coord = limpiar_coordenadas(coord)
+    # Valido que las coordenadas tengan dos digitos
+    if len(coord) != 2:
+        print(msj_error)
+        coord = ingresar_coordenadas(valor_maximo)
+    # Valido que las coordenadas estén compuestas por una letra y un número
+    if coord.isdigit() or coord.isalpha():
+        print(msj_error)
+        coord = ingresar_coordenadas(valor_maximo)
+    # Valido que las coordenadas se encuentren dentro del rango
+    c = interpretar_coordenadas(coord)
+    c1, c2 = c
+    if c1 >= valor_maximo or c2 >= valor_maximo:
+        print("El valor ingresado está fuera de rango.")
+        coord = ingresar_coordenadas(valor_maximo)
+    return coord
 
 
 def editar_matriz(c1, mat_real, mat_incognita):
-    c1f = c1[0]
-    c1c = c1[1]
+    c1f, c1c = c1
     mat_incognita[c1f][c1c] = mat_real[c1f][c1c]
     imprimir_matriz(mat_incognita)
 
 
 def coomparar_matriz(c1, c2, mat_real, mat_incognita):
-    c1f = c1[0]
-    c1c = c1[1]
-    c2f = c2[0]
-    c2c = c2[1]
+    c1f, c1c = c1
+    c2f, c2c = c2
     if mat_incognita[c1f][c1c] != mat_real[c2f][c2c]:
+        print()
         print("¡Incorrecto!")
+        print("")
         sleep(3)
         system("cls")
         mat_incognita[c1f][c1c] = 0
         mat_incognita[c2f][c2c] = 0
         imprimir_matriz(mat_incognita)
+        return False
     else:
+        print("")
         print("¡Correcto!")
+        print("")
+        return True
+
+
+def jugada(jugadas_anteriores, valor_maximo):
+    c = ingresar_coordenadas(valor_maximo)
+    while interpretar_coordenadas(c) in jugadas_anteriores:
+        print("El valor ingresado ya fue encontrado.")
+        c = ingresar_coordenadas(valor_maximo)
+    return interpretar_coordenadas(c)
 
 
 def jugar(mat_real, mat_incognita):
+    valores_ingresados = []
+    val_max = len(mat_real)
     while 0 in [item for fila in mat_incognita for item in fila]:
         # Ingresar coordenadas
-        c1 = ingresar_coordenadas()
-        system("cls")
+        c1 = jugada(valores_ingresados, val_max)
         # Editar matriz
-        editar_matriz(interpretar_coordenadas(c1), mat_real, mat_incognita)
+        system("cls")
+        editar_matriz(c1, mat_real, mat_incognita)
         # Ingresar coordenadas
-        c2 = ingresar_coordenadas()
-        system("cls")
+        c2 = jugada(valores_ingresados, val_max)
+        while c1 == c2:
+            print("No se puede ingresar las mismas coordenadas dos veces.")
+            c2 = jugada(valores_ingresados, val_max)
         # Editar matriz
-        editar_matriz(interpretar_coordenadas(c2), mat_real, mat_incognita)
+        system("cls")
+        editar_matriz(c2, mat_real, mat_incognita)
         # Coomparo resultados
-        coomparar_matriz(interpretar_coordenadas(c1), interpretar_coordenadas(c2), mat_real, mat_incognita)
+        if coomparar_matriz(c1, c2, mat_real, mat_incognita):
+            valores_ingresados.extend([c1, c2])

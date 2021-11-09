3a5ff243d8662a329242adfbfd94da06503f948f
Florencia Quiñonez <fquinonez@uade.edu.ar>
2021-11-09 00:20:47 -0300

    Agrego instrucciones

--- archivo.py
+++ archivo.py
@@ -1,7 +1,7 @@
 # Genera un archivo al terminar el juego
 def generar_archivo(nivel, dificultad, j1, jugadas):
     try:
-        mensaj = '¡¡¡¡¡Felicidades! Gracias por haber jugado!!!!!/n'
+        mensaj = '¡¡¡¡¡Felicidades! Gracias por haber jugado!!!!!\n'
         arch = open('resultado_final.txt', 'wt')
         arch.write(mensaj)
         arch.write(str(j1).upper() + "  ha jugado en modo " + str(dificultad).upper() + " con una matriz de " + str(nivel) + "x" + str(nivel) + '\n')
@@ -13,3 +13,23 @@ def generar_archivo(nivel, dificultad, j1, jugadas):
             arch.close()
         except NameError:
             pass
+
+
+def imprimir_instrucciones():
+    print("***************".center(100))
+    try:
+        arch = open("Instructivo.txt", "rt")
+        linea = arch.readline()
+        while linea:
+            print(linea.rstrip('\n'))
+            linea = arch.readline()
+    except FileNotFoundError as mensaje:
+        print("No se puede acceder a las instrucciones en este momento.", mensaje)
+    except OSError as mensaje:
+        print("No se puede acceder a las instrucciones en este momento.", mensaje)
+    finally:
+        try:
+            arch.close()
+        except NameError:
+            pass
+    print("***************".center(100))
